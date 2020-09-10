596c6e836b7bd4f373dd82cb49737cc81171d5c1
serj <sergejbaturin1985@gmail.com>
2020-09-10 17:32:51 +0500

    Refactoring: -> subscribe base & subscribe options blocks

--- scripts/subscribe.js
+++
@@ -1,7 +0,0 @@
-let card = $(".subscribe__list__item-option");
-
-card.click("click", (e) => {
-  e.currentTarget.style.background = "#fff";
-  e.currentTarget.style.color = "#000";
-  e.currentTarget.style.border = "1px solid #000";
-});
