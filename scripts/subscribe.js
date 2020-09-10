6a498028d39d889a07f3b1dfed1dd5ccbdbbcf6b
serj <sergejbaturin1985@gmail.com>
2020-09-10 12:32:36 +0500

    Changes in subscribe section => circle thumbs, describe width

--- scripts/subscribe.js
+++ scripts/subscribe.js
@@ -1,10 +1,7 @@
-let card = $(".subscribe__list__item");
-let state = true;
+let card = $(".subscribe__list__item-option");
+
 card.click("click", (e) => {
-    e.currentTarget.style.background = "#fff";
-    e.currentTarget.style.color = "#000";
-    e.currentTarget.style.border = "1px solid #000";
-    e.currentTarget.children[1].children[5].children[0].removeAttribute(
-      "checked",
-    );
+  e.currentTarget.style.background = "#fff";
+  e.currentTarget.style.color = "#000";
+  e.currentTarget.style.border = "1px solid #000";
 });
