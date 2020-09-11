76f81cfd04cd1a3ef0c4fd4d5ec52f74c1fe5481
serj <sergejbaturin1985@gmail.com>
2020-09-11 17:22:27 +0500

    Add animation logic in Hero section -> from current element

--- scripts/spinner.js
+++ scripts/spinner.js
@@ -1,7 +1,8 @@
-const addClass = (el, name) => el.classList.add(name)
-const deleteClass = (el, name) => el.classList.remove(name)
-const item = $(".all__icons__list__item")
-const info = $(".all__icons__list__info")[0]
+const addClass = (el, name) => el.classList.add(name);
+const deleteClass = (el, name) => el.classList.remove(name);
+const allIcons = $(".all__icons__list");
+const item = $(".all__icons__list__item");
+const info = $(".all__icons__list__info")[0];
 const infoText = [
   "<ul class='all__icons__list__info__text'><li>Control and manage current issues<br>at the bodyshop</li><li>Record operations and access them<br>anytime when needed</li><li>Schedule the bodyshop work</li></ul>",
   "<ul class='all__icons__list__info__text'><li>Contactless service, electronic paperwork<br>& parts preordering reducing repair time</li><li>Automatic notifications during repair process</li><li>Automatic follow-up,<br>rating requests and up-sales</li></ul>",
@@ -11,70 +12,61 @@ const infoText = [
   "<ul class='all__icons__list__info__text'><li>Convenient and visual staff assignment</li><li>Designed for each and every bodyshop worker with corresponding functionality</li><li>Make supplements, text & voice memos easily to keep your teammates informed</li></ul>",
   "<ul class='all__icons__list__info__text'><li>New assignments are instantly imported<br>in EstVis from insurance companies</li><li>Work Authorizations are automatically generated in accordance with insurance company’s requirements</li><li>One-touch insurer’s documentation upload</li></ul>",
   "<ul class='all__icons__list__info__text'><li>Control current KPIs</li><li>Reveal bottlenecks and optimize your business</li><li>Produce key data reports</li></ul>",
-]
+];
 
-// Классы, динамически добавляемые к HTML 
-const classes = ["all__icons__list__item--choosen", "all__icons__item__img--choosen", "all__icons__item__descr--choosen"]
-
-// Генератор случайных чисел 
-const random = (min, max) => {
-  let rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.round(rand);
-}
+// Классы, динамически добавляемые к HTML
+const classes = [
+  "all__icons__list__item--choosen",
+  "all__icons__item__img--choosen",
+  "all__icons__item__descr--choosen",
+  "all__icons__list__info__text--choosen ",
+];
 
 // Функция добавляет классы к элементам li, svg, p
-const start = () => {
-  let ar = [0,1,2,4,7,6,5,3]
-  let i = 0
-    let timer = setInterval(() => {
+const start = (i) => {
+  let ar = [0, 1, 2, 4, 7, 6, 5, 3];
 
-      i++
-      i === 8 ? i = 0 : i
-      deleteClass($(".all__icons__list__item")[0], classes[0])
-      deleteClass($(".all__icons__item__img")[0], classes[1])
-      deleteClass($(".all__icons__item__descr")[0], classes[2])
-      
-      addClass($(".all__icons__list__item")[ar[i]], classes[0])
-      addClass($(".all__icons__item__img")[ar[i]], classes[1])
-      addClass($(".all__icons__item__descr")[ar[i]], classes[2])
-      
+  let timer = setInterval(() => {
+    
 
-      info.innerHTML = infoText[i]
-      
-      setTimeout(()=> {
-        deleteClass($(".all__icons__list__item")[ar[i]], classes[0])
-        deleteClass($(".all__icons__item__img")[ar[i]], classes[1])
-        deleteClass($(".all__icons__item__descr")[ar[i]], classes[2])
-      }, 2900)
-    }, 3000 )
+    deleteClass($(".all__icons__list__item")[ar[i]], classes[0]);
+    deleteClass($(".all__icons__item__img")[ar[i]], classes[1]);
+    deleteClass($(".all__icons__item__descr")[ar[i]], classes[2]);
 
-    // Останавливает таймер и запускает события при наведении на элемент
-    item.on("mouseenter", (e) => {
-      let infoIndex = e.currentTarget.dataset.info
-      clearInterval(timer)
-        deleteClass($(".all__icons__list__item")[ar[i]], classes[0])
-        deleteClass($(".all__icons__item__img")[ar[i]], classes[1])
-        deleteClass($(".all__icons__item__descr")[ar[i]], classes[2])
-      addClass(e.currentTarget, classes[0])
-      addClass(e.currentTarget.children[0], classes[1])
-      addClass(e.currentTarget.children[1], classes[2])
-      info.innerHTML = infoText[infoIndex]
-    })
+    i++;
+    i === 8 ? (i = 0) : i;
 
-    // Очищает стили при выходе курсора из элемента
-    item.on("mouseleave", (e) => {
-      deleteClass(e.currentTarget, classes[0])
-      deleteClass(e.currentTarget.children[0], classes[1])
-      deleteClass(e.currentTarget.children[1], classes[2])
-    })
-}
+    addClass($(".all__icons__list__item")[ar[i]], classes[0]);
+    addClass($(".all__icons__item__img")[ar[i]], classes[1]);
+    addClass($(".all__icons__item__descr")[ar[i]], classes[2]);
 
-start()
+    info.innerHTML = infoText[i];
 
-$('.all__icons__list').on('mouseleave', () => {
-  addClass($(".all__icons__list__item")[0], classes[0])
-  addClass($(".all__icons__item__img")[0], classes[1])
-  addClass($(".all__icons__item__descr")[0], classes[2])
-  info.innerHTML = infoText[0]
-  start()
-})
+    setTimeout(() => {
+      deleteClass($(".all__icons__list__item")[ar[i]], classes[0]);
+      deleteClass($(".all__icons__item__img")[ar[i]], classes[1]);
+      deleteClass($(".all__icons__item__descr")[ar[i]], classes[2]);
+    }, 2450);
+  }, 2500);
+
+  // Останавливает таймер и запускает события при наведении на элемент
+  item.on("mouseenter", (e) => {
+    let infoIndex = e.currentTarget.dataset.info;
+    clearInterval(timer);
+    deleteClass($(".all__icons__list__item")[ar[i]], classes[0]);
+    deleteClass($(".all__icons__item__img")[ar[i]], classes[1]);
+    deleteClass($(".all__icons__item__descr")[ar[i]], classes[2]);
+    addClass(e.currentTarget, classes[0]);
+    addClass(e.currentTarget.children[0], classes[1]);
+    addClass(e.currentTarget.children[1], classes[2]);
+    info.innerHTML = infoText[infoIndex];
+  });
+};
+
+start(0);
+
+item.on("mouseleave", (e) => {
+  let infoIndex = e.currentTarget.dataset.info;
+  start(infoIndex);
+});
+
